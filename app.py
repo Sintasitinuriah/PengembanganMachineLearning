b9801988b3a0529674b3aea5176070c4600cda06
Sintasitinuriah <72982282+Sintasitinuriah@users.noreply.github.com>
2025-04-12 16:12:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if "history" not in st.session_state:
 
 # ======== LOAD SEMUA MODEL ========
 model_w2v = Word2Vec.load("models/model_w2v.model")
-model_cnn_lstm = load_model("models/cnn_lstm_model.h5")
+model_cnn_lstm = load_model("models/cnn_lstm_model_v2.h5")
 tfidf_vectorizer = joblib.load("models/tfidf_vectorizer.pkl")
 model_nb = joblib.load("models/model_naive_bayes.pkl")
 model_lr = joblib.load("models/model_logistic_regression.pkl")
